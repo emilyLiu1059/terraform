3e86c55bedcf0aca39069c3e12e394151d2e67d1
emilyLiu1059 <73254829+emilyLiu1059@users.noreply.github.com>
2025-08-17 19:08:21 -0400

    Create main.tf

---
+++ modules/s3_bucket/ec2_instance/main.tf
@@ -0,0 +1,45 @@
+resource "aws_security_group" "allow_ssh" {
+  name        = "${var.instance_name}-sg"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.aws_vpc.default.id
+
+  ingress {
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+resource "aws_instance" "this" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name        = var.instance_name
+    Environment = var.environment
+  }
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
+  }
+}
