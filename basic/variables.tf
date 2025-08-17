ad4ae6c63c54f725262b72a1580ce7438d8b17d1
emilyLiu1059 <73254829+emilyLiu1059@users.noreply.github.com>
2025-08-17 17:35:23 -0400

    Create variables.tf

---
+++ basic/variables.tf
@@ -0,0 +1,9 @@
+variable "region" {
+  description = "AWS region"
+  default     = "us-east-1"
+}
+
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  default     = "my-learning-bucket-12345"
+}
