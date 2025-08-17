597c85bb5f50066f385ae4aaaf23a3f9c695dd83
emilyLiu1059 <73254829+emilyLiu1059@users.noreply.github.com>
2025-08-17 19:06:20 -0400

    Update variables.tf

--- basic/variables.tf
+++ basic/variables.tf
@@ -3,7 +3,22 @@ variable "region" {
   default     = "us-east-1"
 }
 
+variable "environment" {
+  description = "Environment tag (e.g., dev, staging, prod)"
+  default     = "dev"
+}
+
 variable "bucket_name" {
   description = "Name of the S3 bucket"
   default     = "my-learning-bucket-12345"
+}
+
+variable "instance_name" {
+  description = "Name tag for the EC2 instance"
+  default     = "learning-ec2"
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  default     = "t2.micro"
 }
