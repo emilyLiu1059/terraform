b5a3da886df7ae56f8842f57acc0b8927a8b2697
emilyLiu1059 <73254829+emilyLiu1059@users.noreply.github.com>
2025-08-17 19:06:00 -0400

    Update main.tf

--- basic/main.tf
+++ basic/main.tf
@@ -5,4 +5,12 @@ provider "aws" {
 module "s3_bucket" {
   source      = "../modules/s3_bucket"
   bucket_name = var.bucket_name
+  environment = var.environment
+}
+
+module "ec2_instance" {
+  source        = "../modules/ec2_instance"
+  instance_name = var.instance_name
+  instance_type = var.instance_type
+  environment   = var.environment
 }
