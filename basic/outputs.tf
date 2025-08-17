126c82766940c8a74df6bc0425e069279b081589
emilyLiu1059 <73254829+emilyLiu1059@users.noreply.github.com>
2025-08-17 19:06:42 -0400

    Update outputs.tf

--- basic/outputs.tf
+++ basic/outputs.tf
@@ -1,3 +1,9 @@
-output "bucket_name" {
-  value = module.s3_bucket.bucket_name
+output "s3_bucket_name" {
+  description = "The name of the S3 bucket"
+  value       = module.s3_bucket.bucket_name
+}
+
+output "ec2_public_ip" {
+  description = "The public IP of the EC2 instance"
+  value       = module.ec2_instance.public_ip
 }
